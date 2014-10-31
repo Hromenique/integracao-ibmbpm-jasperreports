d06c6da5faf788555001c055354affb2a1205409
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2014-10-31 15:31:51 -0300

    Atualização da classe JRTWListDataSource e seu teste

--- src/main/java/br/com/processmind/datasources/JRTWListDataSource.java
+++ src/main/java/br/com/processmind/datasources/JRTWListDataSource.java
@@ -1,32 +1,53 @@
 package br.com.processmind.datasources;
 
+import teamworks.TWObject;
+
 import teamworks.TWList;
+
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
 
+/**
+ * Implementação de uma fonte de dados (Data Source) que fornece dados contidos em um TWList (tipo list utilizado no IBM BPM)
+ * 
+ * @author Hromenique Cezniowscki Leite Batista (hromenique@gmail.com)
+ *
+ */
 public class JRTWListDataSource implements JRDataSource{
 
 	private TWList twList;
-	private int nextPosition;
-	private int listSize;
+	private int index = -1;
 	
-	public JRTWListDataSource(TWList twList) {
-		//this.listSize = twList.getArraySize();
-		this.twList = twList;
-		this.nextPosition = 0;		
+	public JRTWListDataSource(TWList twList) {		
+		this.twList = twList;				
 	}
 	
 	@Override
 	public Object getFieldValue(JRField jrField) throws JRException {
-		// TODO Auto-generated method stub
-		return null;
+		Object fieldValue = null;
+		try {
+			TWObject twObject = (TWObject) this.twList.getArrayData(index);
+			if (twObject != null) {
+				fieldValue = twObject.getPropertyValue(jrField.getName());
+			}
+		} catch (Exception exception) {
+			throw new JRException(exception);
+		}
+		return fieldValue;
 	}
 
 	@Override
 	public boolean next() throws JRException {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		try {
+			if (this.index >= this.twList.getArraySize() - 1) {
+				return false;
+			}
+		} catch (Exception exception) {
+			throw new JRException(exception);
+		}
 
+		this.index++;
+		return true;
+	}
 }
--- src/test/java/br/com/processmind/datasources/JRTWListDataSourceTest.java
+++ src/test/java/br/com/processmind/datasources/JRTWListDataSourceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.*;
 
 import java.util.Date;
 
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
 
 import org.hamcrest.Matchers;
@@ -21,12 +22,13 @@ import teamworks.TWObject;
 
 /**
  * 
- * @author Hromenique Cezniowscki Leite Batista
+ * @author Hromenique Cezniowscki Leite Batista (hromenique@gmail.com)
  *
  */
 
 @RunWith(MockitoJUnitRunner.class)
 public class JRTWListDataSourceTest {
+	
 	@Mock
 	private JRField jrFieldNome;
 	@Mock
@@ -48,15 +50,26 @@ public class JRTWListDataSourceTest {
 	
 	@Before
 	public void init(){
-		MockitoAnnotations.initMocks(this);
-		dataSource = new JRTWListDataSource(twList);
-	}	
+		MockitoAnnotations.initMocks(this);		
+	}
+	
+	@Test(expected=JRException.class)
+	public void testNext_ComTWListIgualANull() throws JRException{
+		
+		dataSource = new JRTWListDataSource(null);
+		dataSource.next();
+	}
 
 	@Test
-	public void testNext_ComVazio() throws Exception {
+	public void testNext_ZeroPosicoes() throws Exception {
+		
 		when(twList.getArraySize()).thenReturn(0);		
 			
-		assertEquals(false, dataSource.next());
+		dataSource = new JRTWListDataSource(twList);
+		//mover para a primeira posição
+		assertEquals(false, dataSource.next()); 
+		//força o movimento além da primeira posição
+		assertEquals(false, dataSource.next()); 
 		
 		verify(twList, Mockito.atLeast(1)).getArraySize();
 	}
@@ -65,6 +78,10 @@ public class JRTWListDataSourceTest {
 	public void testNext_ComUmaPosicao() throws Exception {
 		when(twList.getArraySize()).thenReturn(1);			
 		
+		dataSource = new JRTWListDataSource(twList);
+		//mover para a primeira posição
+		assertEquals(true, dataSource.next());
+		//força o movimento além da primeira posição
 		assertEquals(false, dataSource.next());
 		
 		verify(twList, Mockito.atLeast(1)).getArraySize();
@@ -74,7 +91,12 @@ public class JRTWListDataSourceTest {
 	public void testNext_Com2Posicoes() throws Exception {
 		when(twList.getArraySize()).thenReturn(2);	
 		
+		dataSource = new JRTWListDataSource(twList);
+		//mover para a primeira posição
+		assertEquals(true, dataSource.next());
 		assertEquals(true, dataSource.next());		
+		assertEquals(false, dataSource.next());
+		//força o movimento além da última posição
 		assertEquals(false, dataSource.next());
 		
 		verify(twList, Mockito.atLeast(1)).getArraySize();
@@ -82,19 +104,16 @@ public class JRTWListDataSourceTest {
 	
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testGetFieldValue_Existente() throws Exception {
+	public void testGetFieldValue_ComUmaPosicao() throws Exception {
+		when(twList.getArraySize()).thenReturn(1);	
 		when(twList.getArrayData(0)).thenReturn(twObj1);
 		
-		when(twObj1.getPropertyValue("nome")).thenReturn("Fulano");
-		when(twObj1.getPropertyValue("idade")).thenReturn(24);
-		when(twObj1.getPropertyValue("salario")).thenReturn(1200.0);
-		when(twObj1.getPropertyValue("nascimento")).thenReturn(new Date(2000, 10, 10));
+		prepararMockTWObject(twObj1, "Fulano", 24, 1200.0, new Date(2000, 10, 10));		
+		prepararMockJRField();
 		
-		when(jrFieldNome.getName()).thenReturn("nome");
-		when(jrFieldIdade.getName()).thenReturn("idade");
-		when(jrFieldSalario.getName()).thenReturn("salario");
-		when(jrFieldNascimento.getName()).thenReturn("nascimento");
-		
+		dataSource = new JRTWListDataSource(twList);
+		//mover para a primeira posição
+		dataSource.next();
 		Object fieldValueNome = dataSource.getFieldValue(jrFieldNome);
 		Object fieldValueIdade = dataSource.getFieldValue(jrFieldIdade);
 		Object fieldValueSalario = dataSource.getFieldValue(jrFieldSalario);
@@ -106,30 +125,91 @@ public class JRTWListDataSourceTest {
 		assertThat(fieldValueNascimento, Matchers.instanceOf(Date.class));
 		
 		assertEquals(fieldValueNome, "Fulano");
-		assertEquals(fieldValueNome, 24);
-		assertEquals(fieldValueNome, 1200.0);
-		assertEquals(fieldValueNome, new Date(2000, 10, 10));
+		assertEquals(fieldValueIdade, 24);
+		assertEquals(fieldValueSalario, 1200.0);
+		assertEquals(fieldValueNascimento, new Date(2000, 10, 10));		
 		
-		verify(twList, Mockito.times(1)).getArrayData(0);
+		verify(twList, Mockito.atLeast(1)).getArrayData(0);
 		
-		verify(jrFieldNome, Mockito.times(1)).getName();
-		verify(jrFieldIdade, Mockito.times(1)).getName();
-		verify(jrFieldSalario, Mockito.times(1)).getName();
-		verify(jrFieldNascimento, Mockito.times(1)).getName();	
-		
-		verify(twObj1, Mockito.times(1)).getPropertyValue("nome");
-		verify(twObj1, Mockito.times(1)).getPropertyValue("idade");
-		verify(twObj1, Mockito.times(1)).getPropertyValue("salario");
-		verify(twObj1, Mockito.times(1)).getPropertyValue("nascimento");		
+		verifyMockJRField();		
+		verifyMockTWObject(twObj1);				
 	}
-	
-	@Test
-	public void testGetFieldValue_NaoExistente() throws Exception{		
-		when(jrFieldInvalido.getName()).thenReturn("campoDeNomeInvalido");		
+
+	@Test	
+	@SuppressWarnings("deprecation")
+	public void testGetFieldValue_Com2Posicoes() throws JRException{
+		when(twList.getArraySize()).thenReturn(2);	
+		when(twList.getArrayData(0)).thenReturn(twObj1);
+		when(twList.getArrayData(1)).thenReturn(twObj2);
 		
+		prepararMockTWObject(twObj1, "Fulano", 24, 1200.0, new Date(2000, 10, 10));	
+		prepararMockTWObject(twObj2, "Siclano", 30, 5000.0, new Date(1983, 7, 10));	
+		prepararMockJRField();
+		
+		dataSource = new JRTWListDataSource(twList);
+		//mover para a primeira posição
+		dataSource.next();
+		//mover para a segunda posição
+		dataSource.next();
+		
+		Object fieldValueNome = dataSource.getFieldValue(jrFieldNome);
+		Object fieldValueIdade = dataSource.getFieldValue(jrFieldIdade);
+		Object fieldValueSalario = dataSource.getFieldValue(jrFieldSalario);
+		Object fieldValueNascimento = dataSource.getFieldValue(jrFieldNascimento);
+		
+		assertThat(fieldValueNome, Matchers.instanceOf(String.class));
+		assertThat(fieldValueIdade, Matchers.instanceOf(Integer.class));
+		assertThat(fieldValueSalario, Matchers.instanceOf(Double.class));
+		assertThat(fieldValueNascimento, Matchers.instanceOf(Date.class));
+		
+		assertEquals(fieldValueNome, "Siclano");
+		assertEquals(fieldValueIdade, 30);
+		assertEquals(fieldValueSalario, 5000.0);
+		assertEquals(fieldValueNascimento, new Date(1983, 7, 10));
+		assertEquals("Teste de movimento para posição inválida",false, dataSource.next());
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testGetFieldValue_ComJRFieldInvalido() throws Exception{		
+		when(twList.getArraySize()).thenReturn(1);	
+		when(twList.getArrayData(0)).thenReturn(twObj1);
+		when(jrFieldInvalido.getName()).thenReturn("campoDeNomeInvalido");	
+		prepararMockTWObject(twObj1, "Fulano", 24, 1200.0, new Date(2000, 10, 10));	
+		
+		dataSource = new JRTWListDataSource(twList);
+		dataSource.next();
 		assertNull(dataSource.getFieldValue(jrFieldInvalido));
 		
 		verify(jrFieldInvalido, Mockito.times(1)).getName();
+	}
+
+	private void prepararMockTWObject(TWObject twObj, String nome, int idade, double salario, Date nascimento) {
+		when(twObj.getPropertyValue("nome")).thenReturn(nome);
+		when(twObj.getPropertyValue("idade")).thenReturn(idade);
+		when(twObj.getPropertyValue("salario")).thenReturn(salario);
+		when(twObj.getPropertyValue("nascimento")).thenReturn(nascimento);
+	}
+	
+	private void verifyMockTWObject(TWObject twObj) {
+		verify(twObj, Mockito.times(1)).getPropertyValue("nome");
+		verify(twObj, Mockito.times(1)).getPropertyValue("idade");
+		verify(twObj, Mockito.times(1)).getPropertyValue("salario");
+		verify(twObj, Mockito.times(1)).getPropertyValue("nascimento");
+	}
+
+	private void prepararMockJRField() {
+		when(jrFieldNome.getName()).thenReturn("nome");
+		when(jrFieldIdade.getName()).thenReturn("idade");
+		when(jrFieldSalario.getName()).thenReturn("salario");
+		when(jrFieldNascimento.getName()).thenReturn("nascimento");
+	}
+
+	private void verifyMockJRField() {
+		verify(jrFieldNome, Mockito.times(1)).getName();
+		verify(jrFieldIdade, Mockito.times(1)).getName();
+		verify(jrFieldSalario, Mockito.times(1)).getName();
+		verify(jrFieldNascimento, Mockito.times(1)).getName();
 	}
 
 }
